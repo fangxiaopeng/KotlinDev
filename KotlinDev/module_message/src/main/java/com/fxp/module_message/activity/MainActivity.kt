96b0919adda60e3ecf8b9f27f8033627b9e2b8c8
fangxiaopeng <850899969@qq.com>
2018-09-01 14:34:47 +0800

    update MainActivity.kt

--- KotlinDev/module_message/src/main/java/com/fxp/module_message/activity/MainActivity.kt
+++ KotlinDev/module_message/src/main/java/com/fxp/module_message/activity/MainActivity.kt
@@ -5,9 +5,38 @@ import com.fxp.module_message.R
 
 class MainActivity : BaseActivity() {
 
+    var middleTitle: String?=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun setLayoutId(): Int = R.layout.activity_main
+
+    override fun findView() {
+
+    }
+
+    override fun initData() {
+        middleTitle = "我的消息"
+    }
+
+    override fun initView() {
+        setTopTitle(middleTitle)
+
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun clickLeftBtn() {
+        finish()
+    }
+
+    override fun clickRightBtn() {
+
     }
 
 }
