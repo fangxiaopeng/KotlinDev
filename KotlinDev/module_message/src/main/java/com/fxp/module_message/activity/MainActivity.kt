01f26ad31bcc16b1a09eba6735595ac149b8b930
fangxiaopeng <850899969@qq.com>
2018-09-02 23:11:10 +0800

    update MainActivity.kt

--- KotlinDev/module_message/src/main/java/com/fxp/module_message/activity/MainActivity.kt
+++ KotlinDev/module_message/src/main/java/com/fxp/module_message/activity/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.fxp.module_message.activity
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import com.fxp.module_message.R
+import com.fxp.module_message.adapter.FragmentViewPagerAdapter
+import com.fxp.module_message.fragment.PrivateMsgFragment
 
 class MainActivity : BaseActivity() {
+
+    var mainTab: TabLayout? = null
+
+    var mainViewPager: ViewPager? = null
 
     var middleTitle: String?=""
 
@@ -15,7 +23,8 @@ class MainActivity : BaseActivity() {
     override fun setLayoutId(): Int = R.layout.activity_main
 
     override fun findView() {
-
+        mainTab = findViewById<TabLayout>(R.id.main_tab)
+        mainViewPager = findViewById<ViewPager>(R.id.main_viewpager)
     }
 
     override fun initData() {
@@ -25,6 +34,9 @@ class MainActivity : BaseActivity() {
     override fun initView() {
         setTopTitle(middleTitle)
 
+        initTabLayout()
+
+        initMainViewPager()
     }
 
     override fun initListener() {
@@ -39,4 +51,20 @@ class MainActivity : BaseActivity() {
 
     }
 
+    fun initTabLayout(){
+        mainTab!!.addTab(mainTab!!.newTab().setText("私信"))
+        mainTab!!.addTab(mainTab!!.newTab().setText("评论"))
+        mainTab!!.addTab(mainTab!!.newTab().setText("@我"))
+        mainTab!!.addTab(mainTab!!.newTab().setText("通知"))
+        mainTab!!.setupWithViewPager(mainViewPager)
+    }
+
+    fun initMainViewPager(){
+        val adapter = FragmentViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(PrivateMsgFragment(), "私信")
+        adapter.addFragment(PrivateMsgFragment(), "评论")
+        adapter.addFragment(PrivateMsgFragment(), "@我")
+        adapter.addFragment(PrivateMsgFragment(), "通知")
+        mainViewPager!!.setAdapter(adapter)
+    }
 }
