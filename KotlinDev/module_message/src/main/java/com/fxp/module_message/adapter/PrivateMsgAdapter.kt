65471b776674b1695f3a58dabe8ff8a60b915118
fangxiaopeng <850899969@qq.com>
2018-09-05 16:47:13 +0800

    完成私信列表

--- KotlinDev/module_message/src/main/java/com/fxp/module_message/adapter/PrivateMsgAdapter.kt
+++ KotlinDev/module_message/src/main/java/com/fxp/module_message/adapter/PrivateMsgAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.fxp.module_message.R
 import com.fxp.module_message.model.PrivateMsgModel
@@ -30,10 +31,18 @@ class PrivateMsgAdapter(context: Context?, list: MutableList<PrivateMsgModel>) :
     }
 
     override fun onBindViewHolder(holder: PrivateMsgHolder, position: Int) {
-        holder?.msgContentTxt?.text = list?.get(position)!!.msgContent
+        holder?.userPhotoImageView?.setImageResource(R.mipmap.img_onepiece)
+        holder?.userNameTxt?.text = list?.get(position)!!.userName
+        holder?.lastMsgTimeTxt?.text = list?.get(position)!!.msgTime
+        holder?.lastMsgContentTxt?.text = list?.get(position)!!.msgContent
+        holder?.unreadMsgCount?.text = list?.get(position)!!.msgCount
     }
 
     inner class PrivateMsgHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-        var msgContentTxt: TextView = itemView?.findViewById(R.id.msg_content) as TextView
+        var userPhotoImageView: ImageView = itemView?.findViewById(R.id.iv_user_photo) as ImageView
+        var userNameTxt: TextView = itemView?.findViewById(R.id.tv_user_name) as TextView
+        var lastMsgTimeTxt: TextView = itemView?.findViewById(R.id.tv_last_msg_time) as TextView
+        var lastMsgContentTxt: TextView = itemView?.findViewById(R.id.tv_last_msg_content) as TextView
+        var unreadMsgCount: TextView = itemView?.findViewById(R.id.tv_unread_msg_count) as TextView
     }
 }
