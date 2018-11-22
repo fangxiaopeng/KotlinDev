804476844364c689d20f40d47b52f8bef614ade4
fangxiaopeng <850899969@qq.com>
2018-11-22 18:22:38 +0800

    update MainActivity.kt

--- KotlinDev/app/src/main/java/com/fxp/kotlindev/MainActivity.kt
+++ KotlinDev/app/src/main/java/com/fxp/kotlindev/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test("1");
+        test("2");
+        test("3");
+
+    }
+
+    fun test(str : String){
+        println(str);
     }
 }
